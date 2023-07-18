5da91347f3cb65c9706f30ce3b10fe9160f673f7
ZheyangXu <jishengxzy@hotmail.com>
2023-07-18 21:54:47 +0800

    add base file

---
+++ src/jingwei/infra/typing.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+from typing import  Any, Dict, List, Tuple, Type, Union
+
+import torch
+import numpy as np
+
+
+ObservationType = Type[np.ndarray]
+ObservationsType = Type[List[ObservationType]]
+ValueType = Type[float]
+AdvantageType = Type[float]
+ActionType = Type[Union[int, float, np.ndarray]]
+RewardType = Type[float]
+LossType = Type[Union[float, torch.Tensor]]
