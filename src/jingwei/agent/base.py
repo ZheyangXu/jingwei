c976d1572eec5a9acb58673fd1240fc759ad28f1
ZheyangXu <jishengxzy@hotmail.com>
2024-06-14 08:06:22 +0800

    feat: add agent interface

---
+++ src/jingwei/agent/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from jingwei.infra.typing import *
+from jingwei.transitions.base import TransitionBatch
+
+
+class BaseAgent(ABC):
+    @abstractmethod
+    def get_action(self, observation: ObservationType) -> ActionType:
+        pass
+
+    @abstractmethod
+    def estimate_return(self, transitions: TransitionBatch) -> ValueType:
+        pass
+
+    @abstractmethod
+    def update_step(self, transitions: TransitionBatch) -> None:
+        pass
---
+++ src/jingwei/rollout/base.py
@@ -0,0 +1,4 @@
+
+from jingwei.infra.typing import *
+from jingwei.infra.buffer.base import TransitionBuffer
+from jingwei.infra.data_wrapper import DataWrapper
