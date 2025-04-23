67d52066cec913bdf3eb5c139c88a32625159403
Zheyang XU <jishengxzy@hotmail.com>
2025-04-23 20:39:32 +0800

    Develop (#40)
    * docs: update uml in README
    
    * style: fix some type error in buffer
    
    * refactor: add new package nvwa

--- tests/jingwei/data/test_buffer.py
+++ tests/jingwei/data/test_buffer.py
@@ -13,11 +13,11 @@ class MockBuffer(Buffer):
     def sample(self, batch_size: int) -> Batch:
         raise NotImplementedError("Sample method is not implemented for MockBuffer.")
 
-    def get(self, batch_size: int) -> Batch:
-        raise NotImplementedError("Get method is not implemented for MockBuffer.")
-
     def _get_batch(self, batch_indexies: list[int]) -> Batch:
         raise NotImplementedError("Get batch method is not implemented for MockBuffer.")
+
+    def reset(self) -> int:
+        return 0
 
 
 def test_buffer() -> None:
