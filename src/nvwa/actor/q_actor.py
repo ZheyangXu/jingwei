29f27254bd4309c41c358e70f67a3b6d07311b9b
Zheyang XU <jishengxzy@hotmail.com>
2025-04-24 21:30:18 +0800

    Add BaseActor and QActor (#43)
    * feat: code folder structure
    
    * feat: add Transition
    
    * feat: add BaseActor and QActor

---
+++ src/nvwa/actor/q_actor.py
@@ -0,0 +1,49 @@
+from copy import deepcopy
+from typing import Optional, Self
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from nvwa.actor.base import BaseActor
+
+
+class QActor(BaseActor):
+    def __init__(
+        self,
+        model: nn.Module,
+        optimizer: optim.Optimizer,
+        epsilon: float = 0.1,
+        device: torch.device = torch.device("cpu"),
+        is_target_actor: bool = False,
+    ) -> None:
+        super().__init__(model, optimizer, device)
+        self.epsilon = epsilon
+        self._is_target_actor = is_target_actor
+
+    def get_q_values(self, observation: torch.Tensor) -> torch.Tensor:
+        return self.model(observation)
+
+    def get_max_q_values(
+        self, observation: torch.Tensor, action: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
+        if action:
+            max_q_values = self.model(observation).gather(1, action.astype(torch.int64)).squeeze(1)
+        else:
+            max_q_values = self.model(observation).max(1)
+
+        return max_q_values
+
+    def update_target(self, actor: Self) -> None:
+        self.model.load_state_dict(actor.model.state_dict())
+
+    def soft_update_target(self, actor: Self, tau: float = 0.05) -> None:
+        self.model.load_state_dict(
+            self.model.state_dict() + tau * (actor.model.state_dict() - self.model.state_dict())
+        )
+
+    def clone(self) -> Self:
+        return deepcopy(self)
+
+    def is_target_actor(self) -> bool:
+        return self._is_target_actor
